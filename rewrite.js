99a07300f4a13ae23a1d29ba562f8b705baa8b9e
yash23000 <106523515+yash23000@users.noreply.github.com>
2022-06-21 15:11:40 +0530

    Add files via upload

--- rewrite.js
+++ rewrite.js
@@ -1,11 +1,16 @@
 const fs = require('fs')
 
-fs.writeFile('./sample1.txt','TYNYBAY, INDIA PRIVATE LTD', ()=>{
-  console.log("the file has been modified")
-})
+
 fs.readFile('./sample1.txt',(err,data)=>{
   if(err){
     console.log("the error is ", err)
   }
   console.log(data.toString())
+
+  const result = data.toString().replace('tynybay', 'TYNYBAY, INDIA PRIVATE LTD');
+
+fs.writeFile('./sample1.txt',result, ()=>{
+  console.log("the file has been modified")
 })
+
+})  
